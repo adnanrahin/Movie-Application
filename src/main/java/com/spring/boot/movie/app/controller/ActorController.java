fb553567daf0e7f0eb4b95f832279bb90b5b49e3
Adnan Rahin <adnan.rahin13@gmail.com>
2020-12-16 16:52:14 -0500

    Update

--- src/main/java/com/spring/boot/movie/app/controller/ActorController.java
+++ src/main/java/com/spring/boot/movie/app/controller/ActorController.java
@@ -4,11 +4,11 @@ import com.spring.boot.movie.app.services.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller("/")
+@RequestMapping("/actors")
+@Controller
 public class ActorController {
 
     private ActorService actorService;
@@ -18,7 +18,7 @@ public class ActorController {
         this.actorService = actorService;
     }
 
-    @RequestMapping(value = {"/actors", "/actors/index", "/actors/index.html"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"", "", "/index", "/index.html"}, method = RequestMethod.GET)
     public String getActorList(Model model) {
         model.addAttribute("actors", actorService.findAll());
         return "actors/index";
