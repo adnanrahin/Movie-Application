6f8680244bf4f88280be18fb5c6ee528c1f740ff
Adnan Rahin <adnan.rahin13@gmail.com>
2020-12-17 20:30:00 -0500

    Update

--- src/main/java/com/spring/boot/movie/app/controller/ActorController.java
+++ src/main/java/com/spring/boot/movie/app/controller/ActorController.java
@@ -14,6 +14,8 @@ public class ActorController {
 
     private final CrudService<Actor, Long> actorService;
 
+    /*Hard Coded Result*/
+
     @Autowired
     public ActorController(CrudService<Actor, Long> actorService) {
         this.actorService = actorService;
