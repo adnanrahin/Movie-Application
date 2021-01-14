7bdc1cefb79026fe995b9e8afac70d031633af43
Adnan Rahin <adnan.rahin13@gmail.com>
2021-01-13 22:05:25 -0500

    New Service Created

--- movie-backend/src/main/java/com/spring/boot/movie/app/services/implementaions/CustomerServiceImpl.java
+++ movie-backend/src/main/java/com/spring/boot/movie/app/services/implementaions/CustomerServiceImpl.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-    @Autowired
     private final CustomerRepository customerRepository;
 
     @Autowired
