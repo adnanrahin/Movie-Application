3750e5a74062fea7d0a12bd277ffdb1b6f44d3b0
Adnan Rahin <adnan.rahin13@gmail.com>
2021-01-16 23:44:40 -0500

    Cros Origin issue is fixed

--- movie-backend/src/main/java/com/spring/boot/movie/app/configurations/RestDataConfig.java
+++ movie-backend/src/main/java/com/spring/boot/movie/app/configurations/RestDataConfig.java
@@ -1,32 +1,20 @@
 package com.spring.boot.movie.app.configurations;
 
 
-import com.spring.boot.movie.app.model.Film;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import javax.persistence.EntityManager;
-import javax.persistence.metamodel.EntityType;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
-public class RestDataConfig  extends WebSecurityConfigurerAdapter {
+public class RestDataConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,9 +23,12 @@ public class RestDataConfig  extends WebSecurityConfigurerAdapter {
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new
+        UrlBasedCorsConfigurationSource configuration = new
                 UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
+        CorsConfiguration cors = new CorsConfiguration().applyPermitDefaultValues();
+        cors.setAllowedMethods(Arrays.asList("GET", "POST"));
+        configuration.registerCorsConfiguration("/**", cors);
+        return configuration;
     }
+
 }
