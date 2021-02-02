d21348e11541ef4533773f78346ccd98bc61b08c
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-01 23:45:19 -0500

    Category Test

---
+++ movie-backend/src/test/java/com/spring/boot/movie/app/model/CategoryTest.java
@@ -0,0 +1,24 @@
+package com.spring.boot.movie.app.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @Test
+    void getCategoryId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getLastUpdate() {
+    }
+
+    @Test
+    void getFilms() {
+    }
+}
