37a2c6d3d9392eff0545374e24fae077b682b7e6
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-01 23:48:05 -0500

    CountryTest

---
+++ movie-backend/src/test/java/com/spring/boot/movie/app/model/CountryTest.java
@@ -0,0 +1,29 @@
+package com.spring.boot.movie.app.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryTest {
+
+    @BeforeEach
+    public void setUp(){}
+
+
+    @Test
+    void getCountryId() {
+    }
+
+    @Test
+    void getCountry() {
+    }
+
+    @Test
+    void getLastUpdate() {
+    }
+
+    @Test
+    void getCities() {
+    }
+}
