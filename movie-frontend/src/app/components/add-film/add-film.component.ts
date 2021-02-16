57e19b9cb0321c01497d451975a71a6361269409
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-15 23:20:41 -0500

    enum bugged

--- movie-frontend/src/app/components/add-film/add-film.component.ts
+++ movie-frontend/src/app/components/add-film/add-film.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {MovieFormService} from '../../services/movie-form.service';
+import {Movie, Rating} from '../../model/movie';
+import {Language} from '../../model/language';
+
 
 @Component({
   selector: 'app-add-film',
@@ -11,10 +14,18 @@ export class AddFilmComponent implements OnInit {
 
   addFilmFormGroup: FormGroup;
 
+  @Input() currentMovie: Movie;
+  languages: Language[] = [];
   releaseYears: number[] = [];
 
+  currentRating = Rating;
+  enumKeys = [];
+
+
   constructor(private formBuilder: FormBuilder, private movieFormService: MovieFormService) {
+    this.enumKeys = Object.keys(this.currentRating);
   }
+
 
   ngOnInit(): void {
 
@@ -24,28 +35,43 @@ export class AddFilmComponent implements OnInit {
         description: [''],
         releaseYear: [''],
         rentalDuration: [''],
+        language: [''],
         rentalRate: [''],
         length: [''],
         replacementCost: [''],
         specialFeatures: [''],
-        lastUpdate: ['']
+        lastUpdate: [''],
+        rating: ['']
       })
     });
     this.movieFormService.getCreditCardYears().subscribe(data => {
       this.releaseYears = data;
+    });
+    this.movieFormService.getAllLanguage().subscribe(data => {
+      this.languages = data;
     });
   }
 
   // tslint:disable-next-line:typedef
   onSubmit() {
     console.log('Handing the submit button');
-    console.log(this.addFilmFormGroup.get('film').value);
-  }
+    console.log(this.addFilmFormGroup.get('film.title').value);
+    const currentDate = new Date();
+    const saveMovie = new Movie(0,
+      this.addFilmFormGroup.get('film.title').value,
+      this.addFilmFormGroup.get('film.description').value, this.addFilmFormGroup.get('film.releaseYear').value,
+      this.addFilmFormGroup.get('film.language').value,
+      null,
+      this.addFilmFormGroup.get('film.rentalDuration').value,
+      this.addFilmFormGroup.get('film.rentalRate').value,
+      this.addFilmFormGroup.get('film.length').value, this.addFilmFormGroup.get('film.replacementCost').value,
+      this.addFilmFormGroup.get('film.specialFeatures').value, currentDate,
+      this.addFilmFormGroup.get('film.rating').value,
+      []);
 
-  // tslint:disable-next-line:typedef
-  handleYears() {
-    this.movieFormService.getCreditCardYears().subscribe(data => {
-      this.releaseYears = data;
+
+    this.movieFormService.addNewFilm(saveMovie).subscribe(response => {
+      console.log(response);
     });
   }
 }
--- movie-frontend/src/app/services/movie-form.service.ts
+++ movie-frontend/src/app/services/movie-form.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
+import {Movie} from '../model/movie';
+import {Language} from '../model/language';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieFormService {
 
-  private readonly apiUrl: string = 'http://localhost:8080/api/film';
+  private readonly apiUrl: string = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) {
   }
@@ -22,6 +24,14 @@ export class MovieFormService {
       data.push(theYear);
     }
     return of(data);
+  }
+
+  public getAllLanguage(): Observable<Language[]> {
+    return this.http.get<Language[]>(this.apiUrl + '/language/getAllLanguage');
+  }
+
+  public addNewFilm(movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(this.apiUrl + '/film/save', movie);
   }
 
 }
