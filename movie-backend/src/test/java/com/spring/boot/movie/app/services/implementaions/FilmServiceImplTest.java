dfd86bf19a00f7a2eb5954c316578ca2813c2b1e
adnanrahin <adnan.rahin13@gmail.com>
2022-09-22 01:14:57 -0400

    update

---
+++ movie-backend/src/test/java/com/spring/boot/movie/app/services/implementaions/FilmServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.spring.boot.movie.app.services.implementaions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByTitleContaining() {
+    }
+
+    @Test
+    void findFilmByCategoryId() {
+    }
+
+    @Test
+    void findFilmByActorId() {
+    }
+}
